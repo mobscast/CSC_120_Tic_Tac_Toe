1fd6718ce6e66b574ee819a8c0b050490ed70b8e
Matthew Morton <mobscast12@gmail.com>
2022-07-14 14:42:49 -0400

    Added functinality for play so X and O alternate turns and made it so X or O cannot play in a already played sqaure

--- board.py
+++ board.py
@@ -1,7 +1,41 @@
+from math import fabs
 from turtle import back
 
 
-board = [['-','-','-'],['-','-','-'],['-','-','-']]
+board = [['-','-','-']
+        ,['-','-','-']
+        ,['-','-','-']]
 
-for i in board:
-    print(i)
+player1 = 'X'
+player2 = 'O'
+turnNum = 0
+win = False
+
+# Prints the board
+def printboard():
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+# Gets the Column and Row that the player wants, checks to see if it has already been played and plays it.
+def turn(player):
+    print(player + "'s turn")
+    playFlag = True
+    while playFlag:
+        col = int(input("Enter the column number that you want to play [1-3]")) - 1
+        row = int(input("Enter the row number that you want play [1-3]")) - 1
+        if board[row][col] == "X" or board[row][col] == "O":
+            print("Space has already been taken. Please choose another space.")
+        else:
+            board[row][col] = player
+            playFlag = False
+
+
+# Play
+printboard()
+while win == False:
+    turn(player1)
+    printboard()
+
+    turn(player2)
+    printboard()
