75b72358c4175ad7522094295b0b2b3120d1ca92
Matthew Morton <mobscast12@gmail.com>
2022-07-14 15:31:48 -0400

    added functionality to check if a player wins and daw condition.

--- board.py
+++ board.py
@@ -23,19 +23,59 @@ def turn(player):
     playFlag = True
     while playFlag:
         col = int(input("Enter the column number that you want to play [1-3]")) - 1
+        while col < 0 or col >= 3:
+            print("Invalid column number")
+            col = int(input("Enter the column number that you want to play [1-3]")) - 1
         row = int(input("Enter the row number that you want play [1-3]")) - 1
+        while row < 0 or row >= 3:
+            print("Invalid row number")
+            row = int(input("Enter the row number that you want to play [1-3]")) - 1
         if board[row][col] == "X" or board[row][col] == "O":
             print("Space has already been taken. Please choose another space.")
         else:
             board[row][col] = player
             playFlag = False
 
+# Checks all possible win chances and sets 
+def checkWin(player):
+    for i in range(0, 3):
+        if board[i][0] == player and board[i][1] == player and board[i][2] == player:
+            print(player, "Wins!")
+            winFlag = True
+            break
+        elif board[0][i] == player and board[1][i] == player and board[2][i] == player:
+            print(player, "Wins!")
+            winFlag = True
+            break
+        elif board[0][0] == player and board[1][1] == player and board[2][2] == player:
+            print(player, "Wins!")
+            winFlag = True
+            break
+        elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
+            print(player, "Wins!")
+            winFlag = True
+            break
+        else:
+            winFlag = False
+    return winFlag
 
 # Play
 printboard()
 while win == False:
     turn(player1)
+    turnNum += 1
     printboard()
+    if checkWin(player1):
+        win = True
+        break
+    if turnNum == 9:
+        print("Draw, Game Over!!")
+        win = True
+        break
 
     turn(player2)
+    turnNum += 1
     printboard()
+    if checkWin(player2):
+        win = True
+        break
